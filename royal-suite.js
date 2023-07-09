39bca804d675f93a6bae202be057c3817f6c4408
Santhosh Kumar <iamsanthosh2203@gmail.com>
2023-07-10 02:09:13 +0530

    Updated-Mobile

--- royal-suite.js
+++ royal-suite.js
@@ -53,3 +53,22 @@ Slider();
 AOS.init({
   duration: 1200,
 });
+
+// Mobile
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+const closeBtn = document.getElementById("closeBtn");
+
+menuBtn.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+    menu.classList.add("flex");
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  if (menu.classList.contains("flex")) {
+    menu.classList.remove("flex");
+    menu.classList.add("hidden");
+  }
+});
--- services.js
+++ services.js
@@ -22,3 +22,21 @@ setupWindow("windowGrill", "btnGrill", "grill");
 setupWindow("windowFishing", "btnFishing", "fishing");
 setupWindow("windowSpa", "btnSpa", "spa");
 setupWindow("windowBar", "btnBar", "bar");
+
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+const closeBtn = document.getElementById("closeBtn");
+
+menuBtn.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+    menu.classList.add("flex");
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  if (menu.classList.contains("flex")) {
+    menu.classList.remove("flex");
+    menu.classList.add("hidden");
+  }
+});
