b8aa291bb02f97f7929b8be7a54c2e65438d231a
Santhosh Kumar <iamsanthosh2203@gmail.com>
2023-07-10 00:02:50 +0530

    Mobile-Updated

--- rooms.js
+++ rooms.js
@@ -22,3 +22,20 @@ window.addEventListener("scroll", function () {
 AOS.init({
   duration: 1200,
 });
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+const closeBtn = document.getElementById("closeBtn");
+
+menuBtn.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+    menu.classList.add("flex");
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  if (menu.classList.contains("flex")) {
+    menu.classList.remove("flex");
+    menu.classList.add("hidden");
+  }
+});
