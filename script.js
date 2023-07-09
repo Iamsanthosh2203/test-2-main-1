a1ea454395126c2c859ebeeb6c3d7ef46a1fccbd
Santhosh Kumar <iamsanthosh2203@gmail.com>
2023-07-09 18:37:51 +0530

    mobile

--- script.js
+++ script.js
@@ -216,3 +216,21 @@ function Slider() {
   });
 }
 Slider();
+
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+const closeBtn = document.getElementById("closeBtn");
+
+menuBtn.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+    menu.classList.add("flex");
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  if (menu.classList.contains("flex")) {
+    menu.classList.remove("flex");
+    menu.classList.add("hidden");
+  }
+});
